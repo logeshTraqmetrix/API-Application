e957d96e0ec45fdd9752b0dd25ddd81c73357d09
Logesh Traq <logesh.mg@traqmetrix.com>
2024-12-05 19:27:31 +0530

    api count updating

--- functions/api_application_function/Controllers/stockController.js
+++ functions/api_application_function/Controllers/stockController.js
@@ -1,43 +1,31 @@
-// const catalyst = require('zcatalyst-sdk-node')
-// const QueryGenerator = require('../Static/queryGenerator')
-// const apiMiddleware = require('../Middleware/apiMiddleware')
-
-// const stockDetails = async (req, res) => {
-
-//     apiMiddleware(req, res).then((response) => {
-
-       
-//         const sqlQuery = QueryGenerator("StocksTable", req.query)
-//         const catalystApp = catalyst.initialize(req)
-//         catalystApp.zcql().executeZCQLQuery(sqlQuery).then((response2)=>{
-//             res.status(200).json({ message: "success", data: response2 })
-//         })
-//     }).catch((error1)=>{
-//         console.log('error')
-//         res.send(error1)
-//     })
-
-// }
-
-// module.exports = stockDetails
-
-
-
-
 const catalyst = require('zcatalyst-sdk-node');
 const QueryGenerator = require('../Static/queryGenerator');
 const apiMiddleware = require('../Middleware/apiMiddleware');
 
 const stockDetails = async (req, res) => {
-    apiMiddleware(req, res, async () => {  
+    apiMiddleware(req, res, async () => {
 
         try {
-
-            const userData = req.apiDetails
-            const sqlQuery = QueryGenerator("StocksTable", req.query);
             const catalystApp = catalyst.initialize(req);
-            const response2 = await catalystApp.zcql().executeZCQLQuery(sqlQuery);
-            res.status(200).json({ message: "success", data: response2 });
+            const userData = req.apiDetails
+
+            catalystApp.datastore().table("ApiDetailsTable").updateRow({
+                ROWID: userData.ROWID,
+                stockAvailableCalls: parseInt(userData.stockAvailableCalls) - 1,
+                stockConsumedCalls: parseInt(userData.stockConsumedCalls) + 1
+            }).then(async (response3) => {
+
+                console.log('response from updating the api count', response3)
+
+                const sqlQuery = QueryGenerator("StocksTable", req.query);
+
+                const response2 = await catalystApp.zcql().executeZCQLQuery(sqlQuery);
+                res.status(200).json({ message: "success", data: response2 });
+
+            }).catch((error) => {
+                console.log('error in updating the api count', error)
+            })
+
         } catch (error) {
             console.error('Error fetching stock details:', error);
             res.status(500).send('Server error');
